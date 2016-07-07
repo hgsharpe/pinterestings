ea3d2d8cec8ee113e9fe95a4fbf75df7e7435412
Henry G. Sharpe <henrygsharpe@gmail.com>
2016-07-07 19:08:49 -0400

    Images Not showing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,9 @@ Rails.application.configure do
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => :https,
     :s3_credentials => {
+    :s3_region => 'us-east-1',  
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
