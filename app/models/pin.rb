9b19e75a0e00a772700b3ee319dbc4e6c3a20911
Henry G. Sharpe <henrygsharpe@gmail.com>
2016-07-07 23:31:21 -0400

    Still trying to fix image on Amazon3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,6 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, 
 									:styles => { :medium => "300x300>", :thumb => "100x100>" }
-									:storage => :s3,
-									:bucket => 'mypinterestings',
+									:bucket => 'mypinterestings'
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
